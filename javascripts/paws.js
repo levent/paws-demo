d52743976dbf682f1a3eacf39bac979f3e5909e6
Levent Ali <lebreeze@gmail.com>
2011-01-21 10:02:33 +0000

    Include jquery and check if browser supports WebSockets

---
+++ javascripts/paws.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    if(!("WebSocket" in window)) {
+      alert("I'm afraid your browser does not support WebSockets.\nTry the latest build of Chrome.");
+      return;
+    }
+    
+    
+});
+
